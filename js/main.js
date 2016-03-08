68dd36c5bb2b47fa9cb2314555f0f58b86d8ecd4
Michelle <vand0432@algonquinlive.com>
2016-03-08 14:58:45 -0500

    Set it all up and started coding and forgot about committing

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var $header = $('header');
+var $masthead = $('masthead');
+var $h1 = $('h1');
+var $main = $('main');
+var $p = $('main, p');
+var h2 = $('h2');
+
+
+$header.addClass($masthead);
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
+$main.addClass('wrapper');
+$p.addClass('intro');
+$h2.addClass('slug-head')
